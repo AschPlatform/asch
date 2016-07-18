6077ae778fb8c952e9e4893c7284f8d5e5003696
Lumpo <cnck2011@126.com>
2016-07-18 15:41:52 +0000

    add install npm,asch-cli

--- init/install_deps.sh
+++ init/install_deps.sh
@@ -4,11 +4,14 @@ function main() {
   echo "Updating apt-get ..."
   apt-get update -yqq
 
-  echo "Installing nodejs ..."
-	apt-get install nodejs -yqq && test -f /usr/bin/node || ln -s /usr/bin/nodejs /usr/bin/node
+  echo "Installing nodejs,npm ..."
+  apt-get install npm,nodejs -yqq && test -f /usr/bin/node || ln -s /usr/bin/nodejs /usr/bin/node
 
   echo "Installing sqlite3 ..." 
   apt-get install sqlite3 -yqq
+
+  echo "Installing asch-cli ..."
+  npm install -g asch-cli
 
   echo "Install dependent package done."
 }
