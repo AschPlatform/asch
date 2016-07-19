ca11a886ff8fdf2f611eab9b14c711ac2edd8f0f
sqfasd <sqf1225@foxmail.com>
2016-07-19 10:21:35 +0800

    donnot install npm and asch-cli in configure step

--- init/install_deps.sh
+++ init/install_deps.sh
@@ -5,13 +5,10 @@ function main() {
   apt-get update -yqq
 
   echo "Installing nodejs,npm ..."
-  apt-get install npm,nodejs -yqq && test -f /usr/bin/node || ln -s /usr/bin/nodejs /usr/bin/node
+  apt-get install nodejs -yqq && test -f /usr/bin/node || ln -s /usr/bin/nodejs /usr/bin/node
 
   echo "Installing sqlite3 ..." 
   apt-get install sqlite3 -yqq
-
-  echo "Installing asch-cli ..."
-  npm install -g asch-cli
 
   echo "Install dependent package done."
 }
