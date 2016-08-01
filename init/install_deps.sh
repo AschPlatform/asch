4171112c3dfd89db8bc1cf2161f01253c4e3aaf3
sqfasd <sqf1225@foxmail.com>
2016-08-01 16:07:15 +0800

    add rebuild command for aschd

--- init/install_deps.sh
+++ init/install_deps.sh
@@ -4,8 +4,11 @@ function main() {
   echo "Updating apt-get ..."
   apt-get update -yqq
 
-  echo "Installing nodejs,npm ..."
-  apt-get install nodejs -yqq && test -f /usr/bin/node || ln -s /usr/bin/nodejs /usr/bin/node
+  # echo "Installing nodejs,npm ..."
+  # apt-get install nodejs -yqq && test -f /usr/bin/node || ln -s /usr/bin/nodejs /usr/bin/node
+
+  echo "Installing wget ..."
+  apt-get install wget -yqq
 
   echo "Installing sqlite3 ..." 
   apt-get install sqlite3 -yqq
