aac30622bbd24de9e8abec62ee654b5564e3ca0c
sqfasd <sqf1225@foxmail.com>
2016-08-04 05:35:46 +0800

    add transaction test unittest

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -20,4 +20,62 @@ describe('utils', function () {
 
     done();
   });
+
+  it('transaction sort should be stable', function (done) {
+    var sortBy = function (a, b) {
+      if (a.type != b.type) {
+        return a.type - b.type;
+      }
+      if (a.amount != b.amount) {
+        return a.amount - b.amount;
+      }
+      return a.id.localeCompare(b.id);
+    };
+    function randNumber (min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function randomTrs () {
+      var trs = [];
+      for (var i = 0; i < 100; ++i) {
+        trs.push({
+          type: 1,
+          amount: 1,
+          id: node.randomUsername()
+        });
+      }
+      return trs;
+    }
+    function clone(a) {
+      var b  = [];
+      for (var i in a) {
+        b.push(a[i]);
+      }
+      return b;
+    }
+    var trs = randomTrs();
+    var trs1 = clone(trs);
+    trs1.sort(sortBy);
+    var trs2 = clone(trs1);
+    trs2.sort(sortBy);
+    node.expect(trs1).to.eql(trs2);
+
+    var fs = require('fs');
+    var path = require('path');
+    var trs21 = JSON.parse(fs.readFileSync(path.join(__dirname, './data/trs.21'), 'utf8'));
+    var trs21_1 = clone(trs21);
+    trs21_1.sort(sortBy);
+
+    var trs21_2 = clone(trs21_1);
+    trs21_2.sort(sortBy);
+    node.expect(trs21_1).eql(trs21_2);
+
+    var trs7694 = JSON.parse(fs.readFileSync(path.join(__dirname, './data/trs.7694'), 'utf8'));
+    var trs7694_1 = clone(trs7694);
+    trs7694_1.sort(sortBy);
+
+    var trs7694_2 = clone(trs7694_1);
+    trs7694_2.sort(sortBy);
+    node.expect(trs7694_1).eql(trs7694_2);
+    done();
+  });
 });
