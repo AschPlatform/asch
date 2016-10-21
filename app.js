ed3266f81992572063c6621ddcde29a0760f3388
sqfasd <sqf1225@foxmail.com>
2016-10-21 11:31:16 +0800

    release v1.1.2

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function verifyGenesisBlock(scope, block) {
 function main() {
   process.stdin.resume();
 
-  var version = '1.1.1';
+  var version = '1.1.2';
   program
     .version(version)
     .option('-c, --config <path>', 'Config file path')
