4144f2526ab89c556c9d1b4293fa9b113c42a048
sqfasd <sqf1225@foxmail.com>
2016-10-26 18:46:34 +0800

    fix request memory leak issue caused by new version

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function verifyGenesisBlock(scope, block) {
 function main() {
   process.stdin.resume();
 
-  var version = '1.1.3';
+  var version = '1.1.4';
   program
     .version(version)
     .option('-c, --config <path>', 'Config file path')
