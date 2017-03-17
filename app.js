1ea59c2d902cd0659949096d6b05a7b277b738de
sqfasd <sqf1225@foxmail.com>
2017-03-17 21:12:37 +0800

    bump version

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function verifyGenesisBlock(scope, block) {
 function main() {
   process.stdin.resume();
 
-  var version = '1.2.7';
+  var version = '1.2.8';
   program
     .version(version)
     .option('-c, --config <path>', 'Config file path')
