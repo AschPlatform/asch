327bda80d130319c6cfae67a14ebb6ec26f3fbcc
bycnck <ghostsky2009@gmail.com>
2017-09-24 16:12:23 +0800

    modify DEBUG

--- test/lib/multinodes.js
+++ test/lib/multinodes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var DEBUG = require('debug')('accounts')
+var DEBUG = require('debug')('multinodes');
 var node = require("./../variables.js");
 var async = require('async');
 
@@ -15,7 +15,7 @@ describe("GET /blocks/getHeight in multi nodes", function () {
 
     it("Should be same height", function (done) {
         async.mapSeries(urls, node._getheight, function (err, results) {
-            console.log('heights', results);
+            DEBUG('heights', results);
             if (!err) {
                 let items = new Set(results);
                 node.expect(items.size).to.equal(1);
