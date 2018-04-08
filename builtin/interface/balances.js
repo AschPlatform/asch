ee618ab51f5f5dd65d52dde9bdbb7b41fad5af98
sqfasd <sqf1225@foxmail.com>
2018-04-08 16:18:54 +0800

    add uia and balances interface

---
+++ builtin/interface/balances.js
@@ -0,0 +1,9 @@
+module.exports = function (router) {
+  router.get('/:address', async function (req) {
+
+  })
+
+  router.get('/:address/:currency', async function (req) {
+
+  })
+}
---
+++ builtin/interface/uia.js
@@ -0,0 +1,17 @@
+module.exports = function (router) {
+  router.get('/issuers', async function (req) {
+
+  })
+
+  router.get('/issuers/:name', async function (req) {
+
+  })
+
+  router.get('/assets', async function (req) {
+
+  })
+
+  router.get('/assets/:name', async function (req) {
+
+  })
+}
