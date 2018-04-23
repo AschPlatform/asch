9392fb0b70a82ce7c9127944f3f73c9c60452785
sqfasd <sqf1225@foxmail.com>
2018-04-23 12:59:19 +0800

    change table name from peer-dapp to peer-chain

--- builtin/model/peer-dapp.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  table: 'peer_dapps',
-  tableFields: [
-    { name: 'peerId', type: 'Number', not_null: true, index: true },
-    { name: 'dappId', type: 'String', not_null: true, length: 64, index: true }
-  ]
-}
