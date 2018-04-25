467afa23eb81cfe1c686fae0fb52169a8668dbf9
Osier Yang <agedosier@gmail.com>
2018-04-25 22:07:07 +0800

    init: Refactor the bash scripts
    * Use $(...) instead of the legacy `...`
    * Remove the "function" for function declarations
    * Destroy the useless cat
    * Remove the trailing whitespaces and blank lines

--- init/config_ntp.sh
+++ init/config_ntp.sh
@@ -2,11 +2,11 @@
 #auth:lumpo
 #date:2016-07-15
 
-function install_ntp() {
+install_ntp() {
 	echo "Installing ntp..."
 	apt-get install ntp -yqq
 	apt-get install ntpdate -yqq
-	cnt=`cat /etc/ntp.conf | grep -v '#' | grep "pool.ntp.org prefer"| wc -l`
+	cnt=$(grep -v '#' /etc/ntp.conf | grep "pool.ntp.org prefer" | wc -l)
 	if [ $cnt -eq 0 ];then
 		echo "server pool.ntp.org prefer" >> /etc/ntp.conf
 	fi
@@ -16,13 +16,12 @@ function install_ntp() {
 	/etc/init.d/ntp start
 	if [ $? -eq 0 ];then
 		echo "Configure ntp done."
-	else	
+	else
 		echo "Configure ntp err." && exit 3
 	fi
-	
 }
 
-function main() {
+main() {
 	install_ntp
 }
 
