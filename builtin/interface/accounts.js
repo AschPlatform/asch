88cc1422bf0e0744da7c1cfe70889c03f4b87568
sqfasd <sqf1225@foxmail.com>
2018-04-29 20:46:15 +0800

    support get account by name

--- builtin/interface/accounts.js
+++ builtin/interface/accounts.js
@@ -1,16 +1,18 @@
 module.exports = function (router) {
   router.get('/:address', async function (req) {
-    let address = req.params.address
-    let account = await app.model.Account.findOne({
-      condition: {
-        address: req.params.address
-      }
-    })
+    let condition = {}
+    if (req.params.address.length <= 20) {
+      condition.name = req.params.address
+    } else {
+      condition.address = req.params.address
+    }
+
+    let account = await app.model.Account.findOne({ condition })
 
     let lastBlock = modules.blocks.getLastBlock()
     var ret = {
       account: account,
-      unconfirmedAccount: app.sdb.get('Account', {address: address}),
+      unconfirmedAccount: app.sdb.get('Account', { address: account.address }),
       latestBlock: {
         height: lastBlock.height,
         timestamp: lastBlock.timestamp
