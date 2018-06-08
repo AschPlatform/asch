9df22d8dc07f5923c1f8fd8d136a3c591809d314
liangpeili <liangpeili@foxmail.com>
2018-06-08 14:10:21 +0800

    update shell-build.js

--- shell-build.js
+++ shell-build.js
@@ -1,60 +1,44 @@
-var path = require('path');
-var util = require('util');
-var moment = require('moment');
-var shell = require('shelljs')
-var package = require('./package');
+const shell = require('shelljs');
+const moment = require('moment');
+const path = require('path');
+const package = require('./package');
 
-var format = util.format;
-var buildTime = moment().format('HH:mm:ss DD/MM/YYYY');
-var DEFAULT_BUILD_TIME = '18:07:38 07/06/2018'
+const buildTime = moment().format('HH:mm:ss DD/MM/YYYY');
 
 function build(osVersion, netVersion) {
-  var dir = 'asch-' + osVersion + '-' + package.version + '-' + netVersion;
-  var fullpath = path.join(__dirname, 'build', dir);
-  var cmds = [];
-  cmds.push(format('cd %s && mkdir -p public chains tmp logs bin data', fullpath));
-  cmds.push(format('cp -r package.json aschd init proto app.js src builtin %s', fullpath));
-  if (netVersion != 'localnet') {
-    cmds.push(format('sed -i "s/testnet/%s/g" %s/aschd', netVersion, fullpath));
-    cmds.push(format('cp config-%s.json %s/config.json', netVersion, fullpath));
-    cmds.push(format('cp genesisBlock-%s.json %s/genesisBlock.json', netVersion, fullpath));
+  let dir = `asch-${osVersion}-${package.version}-${netVersion}`;
+  let fullPath = path.join(__dirname, 'build', dir);
+  shell.mkdir('-p', fullPath);
+  shell.cd(fullPath);
+  shell.mkdir('-p', 'public', 'chains', 'tmp', 'logs', 'bin', 'data');
+  shell.cd(__dirname);
+  shell.cp('-r', 'package.json', 'aschd', 'init', 'proto', 'app.js', 'src', 'builtin', fullPath);
+  if ( netVersion !== 'localnet' ) {
+    shell.sed('-i', 'testnet', netVersion, `${fullPath}/aschd`);
+    shell.cp(`config-${netVersion}.json`, `${fullPath}/config.json`);
+    shell.cp(`genesisBlock-${netVersion}.json`, `${fullPath}/genesisBlock.json`);
   } else {
-    cmds.push(format('cp config.json %s/', fullpath));
-    cmds.push(format('cp genesisBlock.json %s/', fullpath));
-    cmds.push(format('cp third_party/sqlite3.exe %s/', fullpath));
+    shell.cp('config.json', `${fullPath}/`);
+    shell.cp('genesisBlock.json', `${fullPath}/`);
+    shell.cp('third_party/sqlite3.exe', `${fullPath}`);
   }
-  if (osVersion == 'linux') {
-    cmds.push(format('cp `which node` %s/bin/', fullpath));
-  }
-  cmds.push(format('cp -r app.js src %s', fullpath));
-  cmds.push(format('cp -r public/dist %s/public/', fullpath));
-  cmds.push(format('cd %s && npm install --production', fullpath));
-  cmds.push(format('cd %s/.. && tar zcf %s.tar.gz %s', fullpath, dir, dir));
 
-  replace 'localnet' by netVersion in files that in (app.js src/\*)
-  replace DEFAULT_BUILT_TIME by buildTime (app.js )
+  if (osVersion === 'linux') {
+    shell.cp(shell.exec('which node'), `${fullPath}/bin/`);
+  }
+
+  shell.cp('-r', 'app.js', 'src', fullPath);
+  //shell.cp('-r', 'public/dist', `${fullPath}/public/`);
+  shell.exec(`cd ${fullPath} && npm install --production`);
+  shell.exec(`cd ${fullPath}/.. && tar zcf ${dir}.tar.gz ${dir}`);
+  shell.exec(`find ./src -type f -print0 | xargs -0 sed -i 's/localnet/${netVersion}/g'`);
+  shell.sed('-i', 'localnet', netVersion, 'app.js');
+  shell.sed('-i', 'DEFAULT_BUILD_TIME', buildTime);
 }
 
-gulp.task('win64-build-local', function () {
-  return build('win64', 'localnet');
-});
-
-gulp.task('linux-build-local', function () {
-  return build('linux', 'localnet');
-});
-
-gulp.task('win64-build-test', function () {
-  return build('win64', 'testnet');
-});
-
-gulp.task('linux-build-test', function () {
-  return build('linux', 'testnet');
-});
-
-gulp.task('win64-build-main', function () {
-  return build('win64', 'mainnet');
-});
-
-gulp.task('linux-build-main', function () {
-  return build('linux', 'mainnet');
-});
+//build('win64', 'localnet');
+build('linux', 'localnet');
+//build('win64', 'testnet');
+//build('linux', 'testnet');
+//build('win64', 'mainnet');
+//build('linux', 'mainnet');
