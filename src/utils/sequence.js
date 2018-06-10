05df57ae165e75b307e331f4a941c5b337d45c05
sqfasd <sqf1225@foxmail.com>
2018-06-10 17:37:22 +0800

    refactor sequence

--- src/utils/sequence.js
+++ src/utils/sequence.js
@@ -1,14 +1,7 @@
 const util = require('util')
 const async = require('async')
 
-function Sequence(config) {
-  this.counter = 1;
-  this.name = config.name;
-
-  this.queue = async.queue(this.tick.bind(this), 1)
-}
-
-Sequence.prototype.tick = (task, cb) => {
+function tick(task, cb) {
   let args = [(err, res) => {
     if (task.done) {
       setImmediate(task.done, err, res)
@@ -21,23 +14,34 @@ Sequence.prototype.tick = (task, cb) => {
   task.worker.apply(task.worker, args)
 }
 
-Sequence.prototype.add = (worker, args, cb) => {
-  let done
-  if (!cb && args && typeof args === 'function') {
-    done = args
-  } else {
-    done = cb
+class Sequence {
+  constructor(config) {
+    this.counter = 1
+    this.name = config.name
+
+    this.queue = async.queue(tick, 1)
   }
-  if (worker && typeof worker === 'function') {
-    const task = { worker, done }
-    if (util.isArray(args)) {
-      task.args = args
+
+  add(worker, args, cb) {
+    let done
+    if (!cb && args && typeof args === 'function') {
+      done = args
+    } else {
+      done = cb
     }
-    task.counter = this.counter++
-    this.queue.push(task)
+    if (worker && typeof worker === 'function') {
+      const task = { worker, done }
+      if (util.isArray(args)) {
+        task.args = args
+      }
+      task.counter = this.counter++
+      this.queue.push(task)
+    }
+  }
+
+  count() {
+    return this.sequence.length
   }
 }
-
-Sequence.prototype.count = () => this.sequence.length
 
 module.exports = Sequence
