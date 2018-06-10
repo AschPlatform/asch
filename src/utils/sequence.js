9dfbcb339296814e868b65f3a74b828a5d2c9014
sqfasd <sqf1225@foxmail.com>
2018-06-10 17:17:20 +0800

    reimplement sequence

--- src/utils/sequence.js
+++ src/utils/sequence.js
@@ -1,62 +1,43 @@
-var util = require('util');
-var extend = require('extend');
+const util = require('util')
+const async = require('async')
 
 function Sequence(config) {
-  var _default = {
-    onWarning: null,
-    warningLimit: 300
-  }
-  _default = extend(_default, config);
-  var self = this;
-  this.sequence = [];
   this.counter = 1;
   this.name = config.name;
 
-  setImmediate(function nextSequenceTick() {
-    if (_default.onWarning && self.sequence.length >= _default.warningLimit) {
-      _default.onWarning(self.sequence.length, _default.warningLimit);
-    }
-    self.__tick(function () {
-      setTimeout(nextSequenceTick, 3);
-    });
-  });
+  this.queue = async.queue(this.tick.bind(this), 1)
 }
 
-Sequence.prototype.__tick = function (cb) {
-  var self = this;
-  var task = this.sequence.shift();
-  if (!task) {
-    return setImmediate(cb);
-  }
-  var args = [function (err, res) {
-    // console.log(self.name + " sequence out " + task.counter + ' func ' + task.worker.name);
-    task.done && setImmediate(task.done, err, res);
-    setImmediate(cb);
+Sequence.prototype.tick = (task, cb) => {
+  let args = [(err, res) => {
+    if (task.done) {
+      setImmediate(task.done, err, res)
+    }
+    setImmediate(cb)
   }];
   if (task.args) {
-    args = args.concat(task.args);
+    args = args.concat(task.args)
   }
-  task.worker.apply(task.worker, args);
+  task.worker.apply(task.worker, args)
 }
 
-Sequence.prototype.add = function (worker, args, done) {
-  if (!done && args && typeof(args) == 'function') {
-    done = args;
-    args = undefined;
+Sequence.prototype.add = (worker, args, cb) => {
+  let done
+  if (!cb && args && typeof args === 'function') {
+    done = args
+  } else {
+    done = cb
   }
-  if (worker && typeof(worker) == 'function') {
-    var task = {worker: worker, done: done};
+  if (worker && typeof worker === 'function') {
+    const task = { worker, done }
     if (util.isArray(args)) {
-      task.args = args;
+      task.args = args
     }
-    task.counter = this.counter++;
-    // console.log(this.name + " sequence in " + task.counter + ' func ' + worker.name);
-    this.sequence.push(task);
+    task.counter = this.counter++
+    this.queue.push(task)
   }
 }
 
-Sequence.prototype.count = function () {
-  return this.sequence.length;
-}
+Sequence.prototype.count = () => this.sequence.length
 
-module.exports = Sequence;
+module.exports = Sequence
