135d2687901b2f909ce5e63a4393f2ee753fdfc6
sqfasd <sqf1225@foxmail.com>
2018-07-05 17:46:52 +0800

    replace placeholder strings before packing the source code

--- build.js
+++ build.js
@@ -12,15 +12,14 @@ function build(osVersion, netVersion) {
   shell.cd(fullPath)
   shell.mkdir('-p', 'public', 'chains', 'tmp', 'logs', 'bin', 'data')
   shell.cd(__dirname)
-  shell.cp('-r', 'package.json', 'aschd', 'init', 'proto', 'app.js', 'src', 'builtin', fullPath)
+  shell.cp('-r', 'package.json', 'aschd', 'init', 'app.js', 'src', fullPath)
   if (netVersion !== 'localnet') {
     shell.sed('-i', 'testnet', netVersion, `${fullPath}/aschd`)
     shell.cp(`config-${netVersion}.json`, `${fullPath}/config.json`)
     shell.cp(`genesisBlock-${netVersion}.json`, `${fullPath}/genesisBlock.json`)
   } else {
-    shell.cp('config.json', `${fullPath}/`)
-    shell.cp('genesisBlock.json', `${fullPath}/`)
-    shell.cp('third_party/sqlite3.exe', `${fullPath}`)
+    shell.cp('config.json', fullPath)
+    shell.cp('genesisBlock.json', fullPath)
   }
 
   if (osVersion === 'linux') {
@@ -28,11 +27,11 @@ function build(osVersion, netVersion) {
   }
 
   shell.cp('-r', 'app.js', 'src', fullPath)
+  shell.exec(`find ${fullPath}/src -type f -print0 | xargs -0 sed -i 's/localnet/${netVersion}/g'`)
+  shell.sed('-i', 'localnet', netVersion, `${fullPath}/app.js`)
+  shell.sed('-i', 'DEFAULT_BUILD_TIME', buildTime, `${fullPath}/app.js`)
   shell.exec(`cd ${fullPath} && npm install --production`)
   shell.exec(`cd ${fullPath}/.. && tar zcf ${dir}.tar.gz ${dir}`)
-  shell.exec(`find ./src -type f -print0 | xargs -0 sed -i 's/localnet/${netVersion}/g'`)
-  shell.sed('-i', 'localnet', netVersion, 'app.js')
-  shell.sed('-i', 'DEFAULT_BUILD_TIME', buildTime, 'app.js')
 }
 
 build(process.argv[2], process.argv[3])
