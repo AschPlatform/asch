57b5186c565e94dab6497eec162f31c2dc713f2e
sqfasd <sqf1225@foxmail.com>
2018-07-11 12:57:01 +0800

    chore: use eslint-config-asch-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,5 @@
 module.exports = {
-  extends: ["airbnb-base"],
-  env: {
-    browser: true,
-    mocha: true,
-    node: true
-  },
+  extends: ['airbnb-base'],
   rules: {
-    semi: [2, "never"],
-    "no-console": "off",
-    "no-undef": 0,
-    "no-new": 0,
-    "no-alert": "off",
-    "no-plusplus": "off",
-    "no-restricted-syntax": 0,
-    "no-await-in-loop": 0,
-    "import/no-dynamic-require": 0,
-    "global-require": 0,
-    "default-case": "off",
-    "prefer-destructuring": 0,
-    "no-continue": 0,
-    "no-param-reassign": 0
   }
 }
