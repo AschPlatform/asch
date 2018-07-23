8cd6f4b19541523fa53a6df8cbf77b6f48722f55
Saidi <sqf1225@foxmail.com>
2018-07-23 20:05:35 +0800

    test(transfer): add benchmark for signed transactions (#218)

---
+++ test/benchmark/transfer-batch.js
@@ -0,0 +1,40 @@
+const lib = require('../lib')
+
+function submitTransactions(trs) {
+  return new Promise((resolve, reject) => {
+    lib.api.put('/transactions/batch')
+      .send({ transactions: trs })
+      .end((err, res) => {
+        if (err) return reject(err)
+        if (!res.body.success) return reject(res.body.error)
+        return resolve(res)
+      })
+  })
+}
+
+function buildTransactions(n) {
+  const trs = []
+  for (let i = 0; i < n; i++) {
+    trs.push(lib.AschJS.transaction.createTransactionEx({
+      secret: lib.GENESIS_ACCOUNT.secret,
+      type: 1,
+      fee: 10000000,
+      args: [
+        lib.randomCoin(),
+        lib.getNormalAccount().address,
+      ],
+    }))
+  }
+  return trs
+}
+
+(async function main() {
+  try {
+    const trs = buildTransactions(10)
+    console.time('time usage')
+    await submitTransactions(trs)
+    console.timeEnd('time usage')
+  } catch (e) {
+    console.log('error:', e)
+  }
+}())
