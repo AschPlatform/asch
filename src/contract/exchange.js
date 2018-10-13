bc5e64193c420ec5f378161184b3076f67f4628d
chengenguan <2630109260@qq.com>
2018-10-13 17:05:20 +0800

    feat(exchange):adjust type,price logic for exchange

--- src/contract/exchange.js
+++ src/contract/exchange.js
@@ -1,3 +1,51 @@
+async function doExchange(sourceCurrency, targetCurrency, bancor, result, context) {
+  // const exchangeFee = Math.floor(result.targetAmount * 0.001)
+  const exchangeFee = result.targetAmount.toNumber() * 0.001
+  const realTargetAmount = result.targetAmount.minus(exchangeFee)
+  // decrease source, increase target
+  if (sourceCurrency === 'XAS') {
+    app.sdb.increase('Account', { xas: -result.sourceAmount }, { address: senderId })
+    app.balances.increase(app.councilAddress, targetCurrency, exchangeFee)
+    app.balances.increase(senderId, targetCurrency, realTargetAmount.toNumber())
+  }
+  if (targetCurrency === 'XAS') {
+    app.balances.decrease(senderId, sourceCurrency, result.sourceAmount)
+    app.sdb.increase('Account', { xas: exchangeFee }, { address: app.councilAddress })
+    app.sdb.increase('Account', { xas: realTargetAmount.toNumber() }, { address: senderId })
+  }
+  let sourcePrecision = 0
+  let targetPrecision = 0
+  let type
+  let price
+  const bancorObj = bancor.getBancorInfo()
+  if (sourceCurrency === bancorObj.money) {
+    type = 'BUY'
+    price = result.targetAmount.toNumber() / result.sourceAmount.toNumber()
+    sourcePrecision = bancorObj.moneyPrecision
+    targetPrecision = bancorObj.stockPrecision
+  } else {
+    type = 'SELL'
+    price = result.sourceAmount.toNumber() / result.targetAmount.toNumber()
+    sourcePrecision = bancorObj.stockPrecision
+    targetPrecision = bancorObj.moneyPrecision
+  }
+  // Record exchange transactions
+  app.sdb.create('BancorExchange', {
+    id: context.trs.id,
+    address: senderId,
+    timestamp: app.util.slots.getTime(),
+    type,
+    owner: bancorObj.owner,
+    source: sourceCurrency,
+    sourcePrecision,
+    target: targetCurrency,
+    targetPrecision,
+    price,
+    targetAmount: result.targetAmount.toString(10),
+    sourceAmount: result.sourceAmount.toString(10),
+  })
+}
+
 module.exports = {
   async exchangeByTarget(sourceCurrency, targetCurrency, targetAmount, bancorInfo) {
     app.validate('amount', String(targetAmount))
@@ -8,51 +56,14 @@ module.exports = {
       targetCurrency, targetAmount, false)
     // Check source account has sufficient balance to handle the exchange
     if (sourceCurrency === 'XAS') {
-      if (this.sender.xas < simulateResult.sourceAmount) return 'Insufficient balance'
+      if (simulateResult.sourceAmount.gt(this.sender.xas)) return 'Insufficient balance'
     } else {
       const balance = app.balances.get(senderId, sourceCurrency)
       if (balance.lt(simulateResult.sourceAmount)) return 'Insufficient balance'
     }
     if (!bancor) return 'Bancor is not ready'
     const result = await bancor.exchangeByTarget(sourceCurrency, targetCurrency, targetAmount, true)
-    const exchangeFee = Math.floor(result.targetAmount * 0.001)
-    const realTargetAmount = result.targetAmount - exchangeFee
-    // decrease source, increase target
-    if (sourceCurrency === 'XAS') {
-      app.sdb.increase('Account', { xas: -result.sourceAmount }, { address: senderId })
-      app.balances.increase(app.councilAddress, targetCurrency, exchangeFee)
-      app.balances.increase(senderId, targetCurrency, realTargetAmount)
-    }
-    if (targetCurrency === 'XAS') {
-      app.balances.decrease(senderId, sourceCurrency, result.sourceAmount)
-      app.sdb.increase('Account', { xas: exchangeFee }, { address: app.councilAddress })
-      app.sdb.increase('Account', { xas: realTargetAmount }, { address: senderId })
-    }
-    let sourcePrecision = 0
-    let targetPrecision = 0
-    const bancorObj = bancor.getBancorInfo()
-    if (sourceCurrency === bancorObj.stock) {
-      sourcePrecision = bancorObj.stockPrecision
-      targetPrecision = bancorObj.moneyPrecision
-    } else {
-      sourcePrecision = bancorObj.moneyPrecision
-      targetPrecision = bancorObj.stockPrecision
-    }
-    // Record exchange transactions
-    app.sdb.create('BancorExchange', {
-      id: this.trs.id,
-      address: senderId,
-      timestamp: app.util.slots.getTime(),
-      type: 'Buy',
-      owner: bancorInfo.owner,
-      source: sourceCurrency,
-      sourcePrecision,
-      target: targetCurrency,
-      targetPrecision,
-      ratio: result.targetAmount / result.sourceAmount,
-      buyed: result.targetAmount,
-      used: result.sourceAmount,
-    })
+    await doExchange(sourceCurrency, targetCurrency, bancor, result, this)
     return null
   },
 
@@ -61,7 +72,7 @@ module.exports = {
     const senderId = this.sender.address
     // Check source account has sufficient balance to handle the exchange
     if (sourceCurrency === 'XAS') {
-      if (this.sender.xas < sourceAmount) return 'Insufficient balance'
+      if (app.util.bignumber(sourceAmount).gt(this.sender.xas)) return 'Insufficient balance'
     } else {
       const balance = app.balances.get(senderId, sourceCurrency)
       if (balance.lt(sourceAmount)) return 'Insufficient balance'
@@ -71,44 +82,7 @@ module.exports = {
       .create(bancorInfo.money, bancorInfo.stock, bancorInfo.owner)
     if (!bancor) return 'Bancor is not ready'
     const result = await bancor.exchangeBySource(sourceCurrency, targetCurrency, sourceAmount, true)
-    const exchangeFee = Math.floor(result.targetAmount * 0.001)
-    const realTargetAmount = result.targetAmount - exchangeFee
-    // decrease source, increase target
-    if (sourceCurrency === 'XAS') {
-      app.sdb.increase('Account', { xas: -result.sourceAmount }, { address: senderId })
-      app.balances.increase(app.councilAddress, targetCurrency, exchangeFee)
-      app.balances.increase(senderId, targetCurrency, realTargetAmount)
-    }
-    if (targetCurrency === 'XAS') {
-      app.balances.decrease(senderId, sourceCurrency, result.sourceAmount)
-      app.sdb.increase('Account', { xas: exchangeFee }, { address: app.councilAddress })
-      app.sdb.increase('Account', { xas: realTargetAmount }, { address: senderId })
-    }
-    let sourcePrecision = 0
-    let targetPrecision = 0
-    const bancorObj = bancor.getBancorInfo()
-    if (sourceCurrency === bancorObj.stock) {
-      sourcePrecision = bancorObj.stockPrecision
-      targetPrecision = bancorObj.moneyPrecision
-    } else {
-      sourcePrecision = bancorObj.moneyPrecision
-      targetPrecision = bancorObj.stockPrecision
-    }
-    // Record exchange transactions
-    app.sdb.create('BancorExchange', {
-      id: this.trs.id,
-      address: senderId,
-      timestamp: app.util.slots.getTime(),
-      type: 'Sell',
-      owner: bancorInfo.owner,
-      source: sourceCurrency,
-      sourcePrecision,
-      target: targetCurrency,
-      targetPrecision,
-      ratio: result.targetAmount / result.sourceAmount,
-      buyed: result.targetAmount,
-      used: result.sourceAmount,
-    })
+    await doExchange(sourceCurrency, targetCurrency, bancor, result, this)
     return null
   },
 
@@ -116,22 +90,12 @@ module.exports = {
     const bancor = await app.util.bancor.create('BCH', 'XAS')
     if (!bancor) return 'Bancor is not ready'
     const balance = await app.balances.get(app.repurchaseAddr, 'BCH')
-    const result = await bancor.exchangeBySource('BCH', 'XAS', app.util.bignumber(balance).toNumber(), true)
-    app.balances.decrease(app.repurchaseAddr, 'BCH', result.sourceAmount)
+    const result = await bancor.exchangeBySource('BCH', 'XAS', balance, true)
+    app.balances.decrease(app.repurchaseAddr, 'BCH', result.sourceAmount.toNumber())
     if (app.buringPoolAddr) {
       app.sdb.createOrLoad('Account', { xas: 0, address: app.buringPoolAddr, name: null })
-      app.sdb.increase('Account', { xas: result.targetAmount }, { address: app.buringPoolAddr })
+      app.sdb.increase('Account', { xas: result.targetAmount.toNumber() }, { address: app.buringPoolAddr })
     }
-    // burningPoolAccount = await app.sdb.load('Account', app.buringPoolAddr)
-    // if (burningPoolAccount) {
-    //   app.sdb.increase('Account', { xas: result.targetAmount }, { address: burningPoolAccount.address })
-    // } else {
-    //   burningPoolAccount = app.sdb.create('Account', {
-    //     address: app.buringPoolAddr,
-    //     xas: result.targetAmount,
-    //     name: null,
-    //   })
-    // }
     return null
   },
 }
