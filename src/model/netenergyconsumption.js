7b255d1ad84c1059441f7e43c8dc432339e77ff8
chengenguan <2630109260@qq.com>
2019-01-04 15:49:21 +0800

    feat(energy):update energy consumption model for smart contract

--- src/model/netenergyconsumption.js
+++ src/model/netenergyconsumption.js
@@ -4,7 +4,9 @@ module.exports = {
     { name: 'tid', type: 'String', length: 64, not_null: true, primary_key: true },
     { name: 'netUsed', type: 'BigInt', default: 0 },
     { name: 'energyUsed', type: 'BigInt', default: 0 },
+    { name: 'fee', type: 'BigInt', default: 0 },
     { name: 'isFeeDeduct', type: 'Number', default: 0 },
     { name: 'height', type: 'BigInt', not_null: true },
+    { name: 'address', type: 'String', length: 50, not_null: true },
   ]
 }
