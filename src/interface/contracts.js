4df26d1adb5b4a10d978b2d0f398346432f2d2dc
eagleHovering <55476243@qq.com>
2019-01-14 19:47:09 +0800

    feat(contract): consume Energy for smart contract

--- src/interface/contracts.js
+++ src/interface/contracts.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 
 const CONTRACT_MODEL = 'Contract'
-const CONTRACT_BASIC_FIELDS = ['id', 'name', 'tid', 'address', 'owner', 'vmVersion', 'desc', 'timestamp']
+const CONTRACT_BASIC_FIELDS = ['id', 'name', 'tid', 'address', 'ownerId', 'vmVersion', 'consumeOwnerEnergy', 'desc', 'timestamp']
 const CONTRACT_RESULT_MODEL = 'ContractResult'
 
 function parseSort(orderBy) {
@@ -28,19 +28,17 @@ function makeCondition(params) {
    */
 async function handleGetResult(req) {
   const tid = req.query.tid
-  assert(tid !== undefined && tid !== null, 'Invalid param \'tid\', can not be null or undefined')
+  assert(tid !== undefined && tid !== null,`Invalid param 'tid', can not be null or undefined`)
   const results = await app.sdb.find(CONTRACT_RESULT_MODEL, { tid })
   if (results.length === 0) {
     throw new Error(`Result not found (tid = '${tid}')`)
   }
   const ret = results[0]
   return {
-    result: {
-      success: ret.success > 0,
-      gas: ret.gas || 0,
-      error: ret.error || '',
-      stateChangesHash: ret.stateChangesHash || '',
-    },
+    success: ret.success > 0,
+    gas: ret.gas || 0,
+    error: ret.error || '',
+    stateChangesHash: ret.stateChangesHash || '',
   }
 }
 
@@ -54,14 +52,25 @@ async function handleActionRequest(req) {
   throw new Error(`Invalid action, ${action}`)
 }
 
+function convertBigintMemberToString(obj) {
+  Object.keys(obj).forEach(key=>{
+    const value = obj[key]
+    const type = typeof value
+    if (type === 'bigint') 
+      obj[key] = String(value)
+    else if (type === 'object')
+      convertBigintMemberToString(value)
+  })
+}
+
 
 module.exports = (router) => {
   /**
    * Query contracts
    * @param condition owner, address, name, orderBy = id:ASC, limit = 20, offset = 0,
-   * orderBy = (timestamp | id | owner):(ASC|DESC)
+   * orderBy = (timestamp | id | ownerId):(ASC|DESC)
    * @returns query result { count,
-   * contracts : [ { id, name, tid, address, owner, vmVersion, desc, timestamp } ] }
+   * contracts : [ { id, name, tid, address, ownerId, vmVersion, desc, timestamp } ] }
    */
   router.get('/', async (req) => {
     if (req.query.action) {
@@ -74,15 +83,15 @@ module.exports = (router) => {
     const orderBy = req.query.orderBy ? req.query.orderBy : 'id:ASC'
 
     const sortOrder = parseSort(orderBy)
-    const { name, owner, address } = req.query
-    const condition = makeCondition({ name, owner, address })
+    const { name, ownerId, address } = req.query
+    const condition = makeCondition({ name, ownerId, address })
     const fields = CONTRACT_BASIC_FIELDS
 
     const count = await app.sdb.count(CONTRACT_MODEL, condition)
     const range = { limit, offset }
     const contracts = await app.sdb.find(CONTRACT_MODEL, condition, range, sortOrder, fields)
 
-    return { count, contracts }
+    return { success: true, count, contracts }
   })
 
 
@@ -96,7 +105,7 @@ module.exports = (router) => {
     const name = req.params.name
     const contracts = await app.sdb.find(CONTRACT_MODEL, { name })
     if (!contracts || contracts.length === 0) throw new Error('Not found')
-    return { contract: contracts[0] }
+    return { success: true, contract: contracts[0] }
   })
 
   /**
@@ -108,19 +117,37 @@ module.exports = (router) => {
    */
   router.get('/:name/states/:stateName/:key', async (req) => {
     const { name, stateName, key } = req.params
-    const state = await app.contract.queryState(name, stateName, key)
-    return { state }
+    const result = await app.contract.queryState(name, stateName, key)
+    convertBigintMemberToString(result)
+    return result
   })
 
   /**
-     * Get state of contract
-     * @param name  name of contract
-     * @param stateName  state name
-     * @returns state value
-     */
+   * Get state of contract
+   * @param name  name of contract
+   * @param stateName  state name
+   * @returns state value
+   */
   router.get('/:name/states/:stateName', async (req) => {
     const { name, stateName } = req.params
-    const state = await app.contract.queryState(name, stateName)
-    return { state }
+    const result = await app.contract.queryState(name, stateName)
+    convertBigintMemberToString(result)
+    return result
   })
+
+
+  /**
+   * Get state of contract
+   * @param name  name of contract
+   * @param method  constant method name
+   * @param args stringified arguments, eg: ["name", 323]
+   * @returns constant method call result
+   */
+    router.get('/:name/constant/:method/:args', async (req) => {
+      const { name, method, args } = req.params
+      const methodArgs = JSON.parse(args)
+      const result = await app.contract.getConstant(name, method, ...methodArgs)
+      convertBigintMemberToString(result)
+      return result
+    })
 }
