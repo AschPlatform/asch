36b2af75d156f4ab5d6417252986af1e6d9645cf
sqfasd <sqf1225@foxmail.com>
2019-07-07 19:57:47 +0800

    bump version & release v1.5.5

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Application = asch.Application
 function main() {
   process.stdin.resume()
 
-  const version = '1.5.4'
+  const version = '1.5.5'
   program
     .version(version)
     .option('-c, --config <path>', 'Config file path')
