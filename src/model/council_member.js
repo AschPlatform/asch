b4154a0dc1aa10ea45783193b43ae014c6d51acf
sqfasd <sqf1225@foxmail.com>
2019-10-29 22:03:42 +0800

    feat(council): adjust the primary_key of the council_members model

--- src/model/council_member.js
+++ src/model/council_member.js
@@ -2,9 +2,9 @@ module.exports = {
   table: 'council_members',
   memory: true,
   tableFields: [
-    { name: 'address', type: 'String', length: 50, primary_key: true, not_null: true },
+    { name: 'name', type: 'String', length: 50, primary_key: true, not_null: true },
+    { name: 'address', type: 'String', length: 50, index: true, not_null: true },
     { name: 'tid', type: 'String', length: 64, unique: true, not_null: true },
-    { name: 'name', type: 'String', length: 50, index: true },
     { name: 'publicKey', type: 'String', length: 64, unique: true },
     { name: 'votes', type: 'BigInt' },
     { name: 'website', type: 'String', length: 256 }
